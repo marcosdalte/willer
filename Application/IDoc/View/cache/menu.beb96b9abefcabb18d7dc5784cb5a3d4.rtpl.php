c8be0f894252f9f09075809dc3ec209334636dbc
william <wborba.dev@gmail.com>
2014-10-03 20:33:55 -0300

    andamento

--- Application/IDoc/View/cache/menu.beb96b9abefcabb18d7dc5784cb5a3d4.rtpl.php
+++
@@ -1,7 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><div role="navigation" id="sidebar" class="col-md-2">
-    <div class="list-group">
-        <a class="list-group-item <?php if( $page_view=='home' ){ ?>active<?php } ?>" href="<?php echo URL_BASE; ?>/home">
-            <span class="glyphicon glyphicon-home"></span> Home
-        </a>
-    </div>
-</div>
