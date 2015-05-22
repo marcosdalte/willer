d9d8194bb50225be4e4df1ab62cb3599f44e370a
william borba <wborba.dev@gmail.com>
2015-05-21 21:10:24 -0300

    atualizando
    Signed-off-by: william borba <wborba.dev@gmail.com>

--- config.php
+++ config.php
@@ -23,6 +23,7 @@ define("URL_NOT_FOUND",Util::str("%s/%s",[URL_BASE,PATH_404]));
 define("QUERY_LIMIT_ROW",15);
 define("SESSION_LIMIT",10);
 define("DB_DEFAULT",DB_MAIN_NAME);
+define("DB_MYSQL",DB_MYSQL_NAME);
 define("DB_LOG",DB_LOG_NAME);
 
 const DATABASE_INFO = [
@@ -35,6 +36,16 @@ const DATABASE_INFO = [
         "DB_PORT" => DB_MAIN_PORT,
 		"DB_AUTOCOMMIT" => 0,
 		"DB_DEBUG" => 0,
+    ],
+    DB_MYSQL => [
+        "DB_DRIVER" => DB_MYSQL_DRIVER,
+        "DB_HOST" => DB_MYSQL_HOST,
+        "DB_NAME" => DB_MYSQL_NAME,
+        "DB_USER" => DB_MYSQL_USER,
+        "DB_PASSWORD" => DB_MYSQL_PASSWORD,
+        "DB_PORT" => DB_MYSQL_PORT,
+        "DB_AUTOCOMMIT" => 0,
+        "DB_DEBUG" => 0,
     ],
     DB_LOG => [
         "DB_DRIVER" => DB_LOG_DRIVER,
