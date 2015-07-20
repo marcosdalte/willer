3a8e90bf8c2ac971a9e7939c57986c3c81a9d02e
William Borba <william.borba@absoluta.net>
2015-07-20 15:18:39 -0300

    system update

--- src/Core/Log.php
+++ src/Core/Log.php
@@ -4,11 +4,7 @@ namespace Core {
     use \Exception as Exception;
 
     trait Log {
-        public static function write($message,$filename = null) {
-        	if (empty($filename)) {
-        		$filename = ROOT_PATH."/log.txt";
-        	}
-
+        public static function write($message,$filename) {
         	file_put_contents($filename,$message,FILE_APPEND);
         }
     }
