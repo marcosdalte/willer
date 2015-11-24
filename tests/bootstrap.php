e69ba5e00dbbd58c91266314003f621d3a09e3cf
William Borba <william.borba@absoluta.net>
2015-11-24 11:56:36 -0200

    update tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ define('DEBUG',true);
 define('URL_PREFIX','/');
 define('REQUEST_URI','/');
 define('ROOT_PATH',dirname(__DIR__).'/src');
-define('DATABASE_PATH',ROOT_PATH.'/database.json');
+define('DATABASE_PATH',dirname(ROOT_PATH).'/tests/database.json');
 define('DATABASE','default');
 
 require(ROOT_PATH.'/Core/Exception/WF_Exception.php');
