c0707572a068d2119e1147dddd5d47fcf3aee112
williamborba <wborba.dev@gmail.com>
2016-02-21 13:57:38 -0300

    url shared between applications

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,13 +1,12 @@
 <?php
 
 require(dirname(__DIR__).'/vendor/autoload.php');
-require('url.php');
 
 define('DEBUG',true);
 define('URL_PREFIX','');
 define('REQUEST_URI',$_SERVER['REQUEST_URI']);
 define('ROOT_PATH',__DIR__);
-define('DATABASE_PATH',ROOT_PATH.'/database.json');
+define('DATABASE_PATH',ROOT_PATH.'/Config/database.json');
 define('DATABASE','default');
 
-new Core\System($url);
+new Core\System();
