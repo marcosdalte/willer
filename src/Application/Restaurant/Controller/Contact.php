56971663a1b46089597a0bae4b4c3269a6ac3473
William Borba <william.borba@absoluta.net>
2016-02-26 13:53:26 -0300

    update Restaurant example

--- src/Application/Restaurant/Controller/Contact.php
+++ src/Application/Restaurant/Controller/Contact.php
@@ -2,9 +2,6 @@
 
 namespace Application\Restaurant\Controller {
     use Core\Controller;
-    use Core\DAO\Transaction;
-    use Core\Util;
-    use Application\Restaurant\Model\Restaurant;
 
     class Contact extends Controller {
         public function __construct($request_method = null) {
