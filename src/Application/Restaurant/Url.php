68d483c811fa34d235688e56bbb5222e0c43a6df
williamborba <wborba.dev@gmail.com>
2016-03-28 03:18:59 -0300

    up

--- src/Application/Restaurant/Url.php
+++ src/Application/Restaurant/Url.php
@@ -4,16 +4,10 @@ namespace Application\Restaurant {
     class Url {
         static public function url() {
             return [
-                '/^\/?$/'                            => ['Restaurant/Home/home',['GET']],
-                '/^\/home\/?$/'                      => ['Restaurant/Home/home',['GET']],
-                '/^\/contact\/?$/'                   => ['Restaurant/Contact/contact',['POST']],
-                '/^\/restaurant\/add\/?$/'           => ['Restaurant/Home/restaurantAdd',['GET','POST']],
-                '/^\/restaurant\/update\/?$/'        => ['Restaurant/Home/restaurantUpdate',['GET']],
-                '/^\/restaurant\/delete\/?$/'        => ['Restaurant/Home/restaurantDelete',['GET']],
-                '/^\/restaurant\/get\/?$/'           => ['Restaurant/Home/restaurantGet',['GET']],
-                '/^\/restaurant\/listing\/?$/'       => ['Restaurant/Home/restaurantListing',['GET']],
-                '/^\/restaurant\/like\/listing\/?$/' => ['Restaurant/Home/restaurantLikeListing',['GET']],
-                '/^\/restaurant\/other-page\/?$/'    => ['Restaurant/Home/otherView',['GET']],
+                '/'           => ['Home:home',['GET'],'home_defaul'],
+                '/home'       => ['Home:home',['GET'],'home_home'],
+                '/contact'    => ['Contact:contact',['POST'],'contact_defaul'],
+                '/restaurant' => ['Home:Dispath',['GET','POST','PUT','DELETE'],'restaurant_dispath'],
             ];
 
             // return [
